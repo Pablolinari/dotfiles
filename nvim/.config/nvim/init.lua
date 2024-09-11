87f84ccda2101e1b030e49b0e8b0e3773a2d7fbe
Pablolinari <paablolinaari@gmail.com>
2024-09-11 22:06:35 +0200

    cambios varios

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-vim.cmd('colorscheme catppuccin-mocha')
+vim.cmd('colorscheme tokyonight')
 vim.o.conceallevel = 2
+vim.api.nvim_set_keymap('n', '<leader>pp', ':Ex ~/.config/nvim/lua/plugins', { noremap = true, silent = true })
 
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 return{
+{
   'projekt0n/github-nvim-theme',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
@@ -8,4 +9,26 @@ return{
     })
 
   end,
+},
+{
+	"catppuccin/nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+      -- ...
+  end,
+	},
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+
 }
