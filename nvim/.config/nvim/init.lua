ca8d4409c8c4bef727092b1b25cf1652b8aeec86
Pablolinari <paablolinaari@gmail.com>
2024-09-12 18:22:13 +0200

    aniado tmux navigation markdown y nuevo keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,5 +3,4 @@ require("config.options")
 require("config.keymaps")
 vim.cmd('colorscheme tokyonight')
 vim.o.conceallevel = 2
-vim.api.nvim_set_keymap('n', '<leader>pp', ':Ex ~/.config/nvim/lua/plugins', { noremap = true, silent = true })
 
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
+----- tab keymaps --------
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
@@ -16,6 +16,12 @@ keymap.set('n', '<c-j>',':wincmd j<CR>')
 keymap.set('n', '<c-h>',':wincmd h<CR>')
 keymap.set('n', '<c-l>',':wincmd l<CR>')
 
+-- Keymap para abrir plugins --
+
+keymap.set('n','<leader>p','<cmd>:edit ~/.config/nvim/lua/<CR>' , {desc = "Abre la configuracion de nvim "})
+
+---harpoon keymaps --- 
+
 local harpoon = require("harpoon")
 
 -- REQUIRED
@@ -23,12 +29,12 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n","<leader-a>",function () harpoon.ui:save() end ,{desc = "Harpoon a file "})
+vim.keymap.set("n","<leader>a",function () harpoon:list():add() end, {desc = "harpoon a fie"})
 
-vim.keymap.set("n", "<leader-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader-4>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
@@ -55,3 +61,32 @@ end
 
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
+
+
+--obsidian keymaps --- 
+vim.keymap.set(
+  "n",
+  "<leader>oc",
+  "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
+  { desc = "Obsidian Check Checkbox" }
+)
+vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
+vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+
+
+-- tmux navigation ---
+
+
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
