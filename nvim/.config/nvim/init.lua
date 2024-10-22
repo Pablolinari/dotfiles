40f91ca80ee525ab7efc3e899e86439ff99bb273
Pablolinari <paablolinaari@gmail.com>
2024-10-22 09:06:05 +0200

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-vim.cmd('colorscheme tokyonight')
+vim.cmd('colorscheme github_dark_high_contrast')
 vim.o.conceallevel = 2
 
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-		  theme='dracula'
+		  theme='horizon'
       },
     })
   end,
