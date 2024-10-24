7b8812c07e5dc836157fb99f22f0a172c4a08456
Pablolinari <paablolinaari@gmail.com>
2024-10-24 12:23:22 +0200

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-vim.cmd('colorscheme github_dark_high_contrast')
+vim.cmd('colorscheme tokyonight-night')
 vim.o.conceallevel = 2
 
