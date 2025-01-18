9438573a7d001c1d82de9b4fceb94ddf141281e2
Pablolinari <paablolinaari@gmail.com>
2025-01-18 11:16:48 +0100

    aniado lspconfig

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -21,7 +21,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tl_ls.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.solargraph.setup({
