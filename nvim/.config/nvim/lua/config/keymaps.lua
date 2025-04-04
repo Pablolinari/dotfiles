9f4a0b8e5a9b600f3ac74391a16f7af273043a54
Pabllinari <paablolinaari@gmail.com>
2025-04-04 16:25:47 +0200

    .

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -40,12 +40,14 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>fh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n","<leader>a",function () harpoon:list():add() end, {desc = "harpoon a fie"})
+vim.keymap.set("n","<leader>ha",function () harpoon:list():add() end, {desc = "harpoon a file"})
 
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end,{desc="clear harpoon buffer"})
+vim.keymap.set("n", "<leader>hd", function() harpoon:list():remove() end,{desc="remove harpoon file"})
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
