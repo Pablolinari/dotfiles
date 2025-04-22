1ec08e0897ab3d2ee268ac4fb61df3509e1ee330
Pablolinari <paablolinaari@gmail.com>
2025-04-22 12:56:05 +0200

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-vim.cmd('colorscheme lackluster-night')
+vim.cmd('colorscheme catppuccin-mocha')
 vim.o.conceallevel = 2
 
