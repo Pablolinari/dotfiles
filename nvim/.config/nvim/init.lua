61fc9f197c50f67037b9023b608442c6d663fb28
Pablo Linari <paablolinaari@gmail.com>
2025-12-27 12:55:17 +0100

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,4 +6,4 @@ vim.cmd(":hi statusline guibg=NONE")
 -- para transparencia 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.cmd('colorscheme afterglow')
+vim.cmd('colorscheme gruber-darker')
