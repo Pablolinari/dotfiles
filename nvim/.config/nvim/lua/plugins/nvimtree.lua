6ee9b15923343e577242318264f8992dd783c9c5
Pablo Linari <paablolinaari@gmail.com>
2026-01-08 22:48:23 +0100

    .

---
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      sort_by = "case_sensitive",
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    })
+  end,
+}
