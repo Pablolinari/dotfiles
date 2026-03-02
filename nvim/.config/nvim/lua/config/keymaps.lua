7a45c890754db2b9e7ce03e0b1e8aea367776e13
Pablo Linari <paablolinaari@gmail.com>
2026-03-02 14:42:40 +0100

    .

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,8 @@ keymap.set("n", "<leader>a", ':suspend <CR>', { desc = "suspend" })
 keymap.set("n", "<leader>o", ':update <CR> :source <CR>', { desc = "source" }) 
 --nvimtree and oil keymaps 
 	
---keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "open oil" }) -- split window vertically
-keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "open oil" }) -- split window vertically
+keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "open oil" }) -- split window vertically
+--keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "open oil" }) -- split window vertically
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
--- nvim/.config/nvim/lua/plugins/togleterm.lua
+++ nvim/.config/nvim/lua/plugins/togleterm.lua
@@ -3,7 +3,7 @@ return{
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-y>]], -- Ctrl + \ para abrir/cerrar
-      direction = 'horizontal',      -- Estilo flotante como lazygit
+      direction = 'float',      -- Estilo flotante como lazygit
       close_on_exit = true,     -- Se cierra al terminar el comando
     })
   end
