d4b3378bb126ad2e881c393b7fa6a89aada8e550
Pablo Linari <paablolinaari@gmail.com>
2026-03-24 13:37:26 +0100

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,7 +7,7 @@ vim.cmd(":hi statusline guibg=NONE")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-vim.cmd('colorscheme vscode')
+vim.cmd('colorscheme oscura-vscode')
 vim.g.adwaita_darker = true -- for darker version
 --cambiar colores  del fondo 
 vim.o.background = 'dark'
