a5b5c8ce789aac1eba2b480d503d12aae5162831
Pablo Linari <paablolinaari@gmail.com>
2026-04-21 19:12:38 +0200

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,10 +6,10 @@ vim.cmd(":hi statusline guibg=NONE")
 -- para transparencia 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme oscura-vscode')
 vim.g.adwaita_darker = true -- for darker version
 --cambiar colores  del fondo 
-vim.o.background = 'light'
+vim.o.background = 'dark'
 
 
 -- La pestaña que SÍ está activa
